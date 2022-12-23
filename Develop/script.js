09a8a20df9dd16d1c354cf80ffb9338497f9ccb6
hasherlaws18 <houstonasherlaws@gmail.com>
2022-12-22 22:41:36 -0600

    changes to code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,17 @@
 // Assignment code here
+var length;
+var number;
+var specialCharacter;
+var upperCase;
+var lowerCase;
+var selecton;
+var lettersUp;
 
-
+const characters = ['numbers', 'letters','character']
+number ='0123456789';
+letters ='abcdefghijklmnopqrstuvwxyz';
+character = "!@#$%^&*)(";
+lettersUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -11,6 +22,73 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+function generatePassword(){
+
+length = prompt("Enter the number of characters you would like for your new password (Selct a number between 8-128)")
+if (!length){
+  alert('Please make a valid entry');
+}
+else if (length < 8 || length > 128)
+{
+  length = prompt('New password must be from 8-128 characters');
+}
+else {
+  number = confirm('Include numbers in your new password');
+  specialCharacter = confirm('Include special characters in your new password');
+  uppercase = confirm('Include uppercase letters?');
+  lowerCase = confirm('Include lowercase letters?');
+};
+ // for all of the above true
+ if (number && specialCharacter && upperCase && lowerCase) {
+  selection = character.concat(number,letters,lettersUp);
+}
+// three options selected
+else if (number && specialCharacter && upperCase) {
+  selection = character.concat(number, lettersUp);
+}
+else if (specialCharacter && upperCase && lowerCase) {
+  selection = character.concat(letters, lettersUp);
+}
+else if (upperCase && lowerCase && number) {
+  selection = lettersUp.concat(letters, number);
+}
+else if (lowerCase && number && specialCharacter) {
+  selection = letters.concat(number, character);
+}
+//two options selected
+else if (specialCharacter && number) {
+  selection = character.concat(number);
+}
+else if (number && upperCase) {
+  selection = number.concat(lettersUp);
+}
+else if (upperCase && lowerCase) {
+  selection = lettersUp.concat(letters);
+}
+else if (lowerCase && number) {
+  selection = letters.concat(number);
+}
+else if (lowerCase && specialCharacter) {
+  selection = letters.concat(character);
+}
+else if (specialCharacter && upperCase) {
+  selection = character.concat(lettersUp);
+}
+//one option
+else if (specialCharacter) {
+  selection = character;
+}
+else if (number) {
+  selection = number;
+}
+else if (lowerCase) {
+  selection = letters;
+}
+else if (upperCase) {
+  selection = lettersUp;
+};
+return selection;
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Password Generator Starter Code
+file:///C:/Users/Houston/bootcamp/Module-3/index.html
+This this the code that I created to make my password Generator.
+![Screenshot_20221222_103128](https://user-images.githubusercontent.com/119140324/209271941-1bf81271-d315-4910-8c4d-5c8283e99f49.png)
